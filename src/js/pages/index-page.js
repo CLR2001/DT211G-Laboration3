830af79210d65797556936fba51686be9312f451
CLR2001 <luro2501@student.miun.se>
2026-02-09 15:38:41 +0100

    first commit

---
+++ src/js/pages/index-page.js
@@ -0,0 +1,26 @@
+export const indexPage = `
+<section class="main-content-index">
+  <h1>Startsida</h1>
+  <section class="album">
+    <h2>Slumpmässiga bilder</h2>
+    <div class="album-wrapper">
+      <img src="https://picsum.photos/300.webp?random=1" alt="Slumpmässig bild">
+      <img src="https://picsum.photos/300.webp?random=2" alt="Slumpmässig bild">
+      <img src="https://picsum.photos/300.webp?random=3" alt="Slumpmässig bild">
+      <img src="https://picsum.photos/300.webp?random=4" alt="Slumpmässig bild">
+      <img src="https://picsum.photos/300.webp?random=5" alt="Slumpmässig bild">
+      <img src="https://picsum.photos/300.webp?random=6" alt="Slumpmässig bild">
+    </div>
+  </section>
+  <section class="buttons-container">
+    <h2>Knappar</h2>
+    <div class="buttons">
+      <button type="button" class="button1">Grön</button>
+      <button type="button" class="button2">Blå</button>
+      <button type="button" class="button3">Röd</button>
+      <button type="button" class="button4">Gul</button>
+    </div>
+  </section>
+</section>
+
+`;
---
+++ src/js/pages/sass-page.js
@@ -0,0 +1,3 @@
+export const sassPage = `
+  <h1>Sass</h1>
+`;
